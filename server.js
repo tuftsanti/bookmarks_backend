da2478e6e9be8e9bca68020aaa62b1a9a01232a0
Andy Riecker <ariecker@gmail.com>
2020-07-13 16:07:59 -0400

    mussed with mongo

--- server.js
+++ server.js
@@ -4,14 +4,17 @@ const app = express();
 const mongoose = require('mongoose');
 const cors = require('cors');
 // const methodOverride = require('method-override');
+const bodyParser = require('body-parser');
+
 
 //GLOBALS
 require('dotenv').config() 
 const PORT = process.env.PORT || 3000;
-const MONGOURL = process.env.MONGODB_URL || 'mongodb://localhost:27017/bookmarks';
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/';
 const whitelist = [
     'http://localhost:1985',
-    'andysbookmarks.herokuapp.com'
+    'http://localhost:3000',
+    'https://andysbookmarks.herokuapp.com'
 ];
 const corsOptions = {
     origin: function (origin, callback) {
@@ -27,13 +30,16 @@ const corsOptions = {
 //DB CONNECTION
 mongoose.connection.on('error', (err) => console.log(err.message + ' is Mongod not running?'));
 mongoose.connection.on('disconnected', () => console.log('mongo disconnected'));
-mongoose.connect(MONGOURL, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true,
+    useCreateIndex: true });
 mongoose.connection.once('open', () => console.log('connected to mongoose...'));
 
 // MIDDLEWARE
 app.use(cors(corsOptions));
 app.use(express.json());
 // app.use(methodOverride('_method'))
+app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: false }))
 
 // CONTROLLERS/ROUTES
 const bookmarksController = require('./controllers/bookmarks.js');
